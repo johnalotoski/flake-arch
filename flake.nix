259d3f916cfcc46b6907a82c2254f5bf91b76c3b
John Lotoski <john.lotoski@iohk.io>
2022-08-10 13:55:55 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Flake architecture selection";
+  # A nix flake which simply outputs a systems list.
+  # Flakes which utilize a systems list can be parameterized with this flake as an input.
+  # When needed, this flake input can be overridden to a different system.
+  #
+  # Example:
+  # Overriding the flake input to describe only a single system for CI and to
+  # work around errors like https://github.com/NixOS/nix/issues/4265
+
+  outputs = { self }: {
+    systems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+  };
+}
